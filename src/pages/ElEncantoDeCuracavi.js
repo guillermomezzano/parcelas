1defdd234c49d072ee7113885529a9e71363826b
guillermomezzano <guillermo.mezzano04@gmail.com>
2023-04-13 18:01:13 -0400

    el hanamichi se la come

--- src/pages/ElEncantoDeCuracavi.js
+++ src/pages/ElEncantoDeCuracavi.js
@@ -41,7 +41,7 @@ const ElEncantoDeCuracavi = () => {
 
         <div>
           <div className="conteinerParcelaTitle">
-            <h1>el encanto de curacaví</h1>
+            <h1>el hanamichi se la come</h1>
             <hr></hr>
             <h4>CURACAVÍ</h4>
             <p>
